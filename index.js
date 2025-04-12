4c0ce6e32cf7466253c1997d1036cff0081efb78
Andres Zenteno <azenteno74@gmail.com>
2025-04-12 15:26:52 -0400

    feat: update systemMessage, userMessage, chatGptMessages

--- index.js
+++ index.js
@@ -205,25 +205,31 @@ app.post('/smart-summary', async (req, res) => {
       const systemMessage = {
         role: 'system',
         content: `# IDENTITY and PURPOSE
-  You are an expert content summarizer. You take content in and output a Markdown formatted summary using the format below.
+  As an organized, high-skill content summarizer, your role is to extract the most relevant topics from a video transcript and provide a structured summary using bullet points and lists of definitions for each subject.
+  Your goal is to help the user understand the content quickly and efficiently.
+  You take content in and output a Markdown formatted summary using the format below.
   Take a deep breath and think step by step about how to best accomplish this goal using the following steps.
+  
   # OUTPUT SECTIONS
-  - Combine all of your understanding of the content into a single, 20-word sentence in a section called ONE SENTENCE SUMMARY:.
-  - Output the 10 most important points of the content as a list with no more than 16 words per point into a section called MAIN POINTS:.
-  - Output a list of the 5 best takeaways from the content in a section called TAKEAWAYS:.
+  - Combine all of your understanding of the content into a single, 20-word sentence in a section called ## One Sentence Summary:.
+  - Output the 10 most important points of the content as a list with no more than 16 words per point into a section called ## Main Points:.
+  - Output a list of the 5 best takeaways from the content in a section called ## Takeaways:.
+  
   # OUTPUT INSTRUCTIONS
-  - Create the output using the formatting above.
   - You only output human readable Markdown.
+  - Use a simple and clear language
+  - Create the output using the formatting above.
   - Output numbered lists, not bullets.
   - Do not output warnings or notes—just the requested sections.
   - Do not repeat items in the output sections.
   - Do not start items with the same opening words.
+  - To ensure the summary is easily searchable in the future, keep the structure clear and straightforward.
   # INPUT:
   INPUT:
   `};
       const userMessage = {
         role: 'user',
-        content: `In a simple and clear language, summarize this YouTube transcript, and highlight the main ideas:\n\n${rawTranscript}`
+        content: `${rawTranscript}`
       };
       const chatGptMessages = [systemMessage, userMessage];
       const openaiResponse = await axios.post(
