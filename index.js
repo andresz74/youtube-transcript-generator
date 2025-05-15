684108c9c6376795629745fab7eea5deb0470254
Andres Zenteno <azenteno74@gmail.com>
2025-05-14 22:03:59 -0400

    fix: format tags

--- index.js
+++ index.js
@@ -1151,7 +1151,8 @@ description: |
   ${metadata.description}
 image: '${metadata.image}
 duration: ${metadata.duration}
-tags: ${metadata.tags.map(tag => `  - ${tag}`).join('\n')}
+tags: 
+${metadata.tags.map(tag => `  - ${tag}`).join('\n')}
 canonical_url: ${metadata.canonical_url}
 author: ${metadata.author}
 video_author: ${metadata.video_author}
